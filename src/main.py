2c6c18a71e9d34c487a40c94010506d010ef0658
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-03-26 19:24:36 +0000

    changed time for token

--- src/main.py
+++ src/main.py
@@ -140,7 +140,7 @@ def login():
         return make_response("Could not verify", 401, {"WWW-Authenticate" : "Basic realm='Login required!'" })
         
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({"public_id" : user.public_id, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=15)},  'secret')
+        token = jwt.encode({"public_id" : user.public_id, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},  'secret')
         return jsonify({"token": token.decode("UTF-8")})
 
     return make_response("Could not verify", 401, {"WWW-Authenticate" : "Basic realm='Login required!'" })
